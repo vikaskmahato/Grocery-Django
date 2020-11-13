b38a8582fe884f5e70dd1c564281ed4826762491
vikaskmahato <vikas938@outlook.com>
2020-11-13 23:11:08 +0530

    third commit

--- grocery/grocery/settings.py
+++ grocery/grocery/settings.py
@@ -123,4 +123,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images/')
