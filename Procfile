6df847fd9e72e04db525489e08db316bdb93068d
vikaskmahato <vikas938@outlook.com>
2020-11-14 14:39:14 +0530

    sixth commit

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web:gunicorn grocery.wsgi --log-file -
+web:gunicorn grocery.grocery.wsgi --log-file -
--- grocery/grocery/wsgi.py
+++ grocery/grocery/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'grocery.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'grocery.grocery.settings')
 
 application = get_wsgi_application()
