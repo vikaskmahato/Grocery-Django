20533afef2af33cce4656e0820638ebe7122ebaa
vikaskmahato <vikas938@outlook.com>
2020-11-14 12:17:47 +0530

    fourth commit

--- grocery/grocery/settings.py
+++ grocery/grocery/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'u91v#94&ba%vit8+g7f!xkspeuj9#r5l-+(=tallj@^#+@uri='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','sampledomain.com']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- grocery/grocery/urls.py
+++ grocery/grocery/urls.py
@@ -19,11 +19,16 @@ from foods import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.views.static import serve
+from django.conf.urls import url
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name="home"),
     path('contact/',views.contact,name="contact"),
     path('items/',include('foods.urls')),
     path('accounts/',include('fusers.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
