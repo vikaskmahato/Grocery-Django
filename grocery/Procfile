9e2116f7b52cc03f87e74ab1540a9dcac400f928
vikaskmahato <vikas938@outlook.com>
2020-11-15 13:28:52 +0530

    final commit

--- grocery/Procfile
+++ grocery/Procfile
@@ -1,1 +1,1 @@
-web:gunicorn grocery.grocery.wsgi:application --log-file -
+web: gunicorn grocery.wsgi:application --log-file -
--- grocery/grocery/settings.py
+++ grocery/grocery/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -79,8 +81,8 @@ WSGI_APPLICATION = 'grocery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ciba',
     }
 }
 
@@ -125,3 +127,5 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'images/')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+django_heroku.settings(locals())
