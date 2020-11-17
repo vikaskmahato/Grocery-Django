f41587d17f1dddb5aa30252014eb4e57456dcffe
vikaskmahato <vikas938@outlook.com>
2020-11-17 21:07:44 +0530

    final commit

--- grocery/grocery/settings.py
+++ grocery/grocery/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'u91v#94&ba%vit8+g7f!xkspeuj9#r5l-+(=tallj@^#+@uri='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','grocery-django.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
